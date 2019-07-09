741a9ee67baa6b64ceaf6b2e5b9402f139b3624c
kjellknapen <knapenkjell@gmail.com>
2019-07-09 17:00:29 +0200

    pygrounds startup

--- sitegrounds/views.py
+++ sitegrounds/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Welcome to my pygrounds. This is the start of my website")
