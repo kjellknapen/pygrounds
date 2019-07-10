e1d0c348b4ad202c9073c5e65542087a41ee37f0
kjellknapen <knapenkjell@gmail.com>
2019-07-10 11:33:09 +0200

    fix?

--- pygrounds/wsgi.py
+++ pygrounds/wsgi.py
@@ -6,9 +6,15 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
-
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pygrounds.settings')
+import sys
+
+path='/home/deploybot/pygrounds.knapenkjell.com/'
+
+if path not in sys.path:
+  sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'pygrounds.settings'
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
